87ead6915531275158ac2ec9abeecb47204b6e18
OunissiDhiaEddine <dhia.ounissi@outlook.com>
2024-04-24 20:46:08 +0100

    fixed cors , now api works locally

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express =require ('express') ;
+const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { MONGO_URI } = require('./serverconfig');
 require('dotenv').config();
 
@@ -7,15 +8,11 @@ require('dotenv').config();
 const postsRoutes = require('./api/posts');
 const authRoutes = require('./api/auth');
 
-
-
-
-
-
-
-
 const app = express();
 
+// Use cors and express.json middleware
+app.use(cors());
+app.use(express.json()); 
 
 // Connecting L DZEstate database fi mongodb atlas 
 mongoose.connect(MONGO_URI,
@@ -26,10 +23,8 @@ mongoose.connect(MONGO_URI,
     .then(() => console.log('Connected to database'))
     .catch(err => console.log(err));
 
-
 // 7at lhna routes and apis 
 app.use('/api/posts', postsRoutes);
-app.use(express.json()); 
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
