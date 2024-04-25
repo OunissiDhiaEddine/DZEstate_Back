88d35d5c445351d7ff243e886baca08ea3347601
OunissiDhiaEddine <dhia.ounissi@outlook.com>
2024-04-25 02:25:46 +0100

    reverted

--- api/login.js
+++ api/login.js
@@ -7,36 +7,31 @@ const connectDb = require('../connectDb');
 
 module.exports = cors(async (req, res) => {
   await connectDb();
-  
-  if (req.method === 'POST') {
-    const { email, password } = await req.json();
+  const { email, password } = req.body;
 
-    let user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ msg: 'Invalid Credentials' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ msg: 'Invalid Credentials' });
-    }
-
-    const payload = {
-      user: {
-        id: user.id,
-      },
-    };
-
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: 360000 },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } else {
-    res.status(405).send('Method not allowed');
+  let user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ msg: 'Invalid Credentials' });
   }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ msg: 'Invalid Credentials' });
+  }
+
+  const payload = {
+    user: {
+      id: user.id,
+    },
+  };
+
+  jwt.sign(
+    payload,
+    process.env.JWT_SECRET,
+    { expiresIn: 360000 },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    }
+  );
 });
--- api/register.js
+++ api/register.js
@@ -7,39 +7,34 @@ const connectDb = require('../connectDb');
 
 module.exports = cors(async (req, res) => {
   await connectDb();
-  
-  if (req.method === 'POST') {
-    const { username, email, password } = await req.json();
+  const { username, email, password } = req.body;
 
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ msg: 'User already exists' });
-    }
-
-    user = new User({
-      username,
-      email,
-      password,
-    });
-
-    await user.save();
-
-    const payload = {
-      user: {
-        id: user.id,
-      },
-    };
-
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: 360000 },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } else {
-    res.status(405).send('Method not allowed');
+  let user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).json({ msg: 'User already exists' });
   }
+
+  user = new User({
+    username,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const payload = {
+    user: {
+      id: user.id,
+    },
+  };
+
+  jwt.sign(
+    payload,
+    process.env.JWT_SECRET,
+    { expiresIn: 360000 },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    }
+  );
 });
